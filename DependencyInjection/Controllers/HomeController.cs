114c873c0775f77adcc538236c036e92f6c0b401
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-04-03 22:21:14 +0200

    First part of the example.

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjection.Models;
+using DependencyInjection.Infrastructure;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController: Controller
+    {
+        public IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Products);
+    }
+}
